583b7a368dada86a6a334c8c045f77c990a1c0cc
JoaoVMont <joaovmt1905@gmail.com>
2021-11-30 13:50:49 -0300

    Requisito 8

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
     const trArr = ['Descrição', 'Tag',
       'Método de pagamento', 'Valor', 'Moeda',
       'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     return (
       <table>
-        <tr>
-          {trArr.map((tr) => <th key={ tr }>{tr}</th>)}
-        </tr>
+        <thead>
+          <tr>
+            {trArr.map((tr) => <th key={ tr }>{tr}</th>)}
+          </tr>
+        </thead>
+        { expenses.map((expense) => (
+          <tbody key={ expense.id }>
+            <tr>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.exchangeRates[expense.currency].name.split('/')[0]}</td>
+              <td>
+                { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                { parseFloat((expense.value)
+                * expense.exchangeRates[expense.currency].ask).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>Editar/Excluir</td>
+            </tr>
+          </tbody>
+        ))}
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
